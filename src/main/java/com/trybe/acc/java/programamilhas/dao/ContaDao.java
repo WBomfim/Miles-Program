439f2de6ad828c8fae85fd5340288f5dce2917d3
WBomfim <willianbomfim@hotmail.com>
2023-04-03 01:50:13 -0300

    fix: Corrigido as queries de busca de saldo e extrato

--- src/main/java/com/trybe/acc/java/programamilhas/dao/ContaDao.java
+++ src/main/java/com/trybe/acc/java/programamilhas/dao/ContaDao.java
@@ -22,7 +22,7 @@ public class ContaDao {
    * 
    */
   public List<Lancamento> buscaExtrato(Integer idUsuario) {
-    String hql = "SELECT l FROM Lancamento l WHERE l.idUsuario = :idUsuario";
+    String hql = "SELECT l FROM Lancamento l WHERE l.usuario.id = :idUsuario";
     return entityManager.createQuery(hql, Lancamento.class)
         .setParameter("idUsuario", idUsuario)
         .getResultList();
@@ -33,11 +33,20 @@ public class ContaDao {
    * 
    */
   public SaldoResult buscaSaldo(Integer idUsuario) {
-    String hql = "SELECT l.id, l.login, SUM(l.valor) "
-        + "FROM Lancamento l WHERE l.idUsuario = :idUsuario";
-    return entityManager.createQuery(hql, SaldoResult.class)
+    String hql = "SELECT l.usuario.id, l.usuario.login, "
+        + "SUM(l.valor) AS saldo "
+        + "FROM Lancamento l "
+        + "WHERE l.usuario.id = :idUsuario ";
+
+    Object[] row = entityManager.createQuery(hql, Object[].class)
         .setParameter("idUsuario", idUsuario)
         .getSingleResult();
+
+    Integer userId = (Integer) row[0];
+    String login = (String) row[1];
+    Long saldo = (Long) row[2];
+
+    return new SaldoResult(userId, login, saldo.intValue());
   }
   
 }
